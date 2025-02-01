f4f530ec8a95c2f240555768dba93b7e446a4939
cengaver <selim_koyuncu@hotmail.com>
2025-02-01 03:41:06 +0300

    Create EtsySumSalesListings.user.js

---
+++ EtsySumSalesListings.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Etsy Sum Sales Listings
+// @namespace    https://github.com/cengaver
+// @version      0.1
+// @description  Etsy Sum Sales Listings
+// @author       Cengaver
+// @match        https://www.etsy.com/your/shops/me/tools/listings/*
+// @grant        GM_addStyle
+// @icon         https://www.google.com/s2/favicons?domain=etsy.com
+// @downloadURL  https://github.com/cengaver/EtsyScript/raw/refs/heads/main/EtsySumSalesListings.user.js
+// @updateURL    https://github.com/cengaver/EtsyScript/raw/refs/heads/main/EtsySumSalesListings.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let isUpdating = false;
+    const observer = new MutationObserver(mutations => {
+        if (!isUpdating) sumSalesListing();
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    function sumSalesListing() {
+        isUpdating = true;
+        observer.disconnect(); // Güncelleme sırasında gözlemciyi durduruyoruz
+        const items = document.querySelectorAll('.wt-block-grid__item');
+        let sumSales = 0;
+        items.forEach(item => {
+            const salesElements = item.querySelectorAll('.card-meta-row-item.text-gray-lighter.selected-color');
+            salesElements.forEach(el => {
+                if (el.innerText.includes("sales")) {
+                    const match = el.innerText.match(/\d+/);
+                    if (match) sumSales += Number(match[0]);
+                }
+            });
+        });
+        const sumSalesElement = document.querySelector("#page-region > div > div > header > div.wt-pl-xs-2.wt-pr-xs-2.wt-pl-lg-3.wt-pr-lg-3 > div > div.wt-flex-xs-1.wt-hide-xs.wt-show-lg > h1");
+        if (sumSalesElement) {
+            let saleslabel = document.getElementById('etsy-sales-sum');
+            if (saleslabel) {
+                saleslabel.innerHTML = `<span class="text-gray-lighter">${sumSales} Satış</span>`;
+            } else {
+                saleslabel = document.createElement('div');
+                saleslabel.id = 'etsy-sales-sum';
+                saleslabel.innerHTML = `<span class="text-gray-lighter">${sumSales} Satış</span>`;
+                sumSalesElement.appendChild(saleslabel);
+            }
+        }
+        console.log("sumSales", sumSales);
+        observer.observe(document.body, { childList: true, subtree: true });
+        isUpdating = false;
+    }
+
+    sumSalesListing();
+})();
