369a4b86b5bb901e2c3e75e99000a17dabdddb6c
cengaver <selim_koyuncu@hotmail.com>
2025-03-18 22:52:26 +0300

    Create openLinksSequentially.user.js

---
+++ openLinksSequentially.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Open Links Sequentially for ETSY ad
+// @version      1.0
+// @description  Open all matching links with a 1-second delay
+// @namespace    https://github.com/cengaver
+// @author       Cengaver
+// @match        https://www.etsy.com/your/shops/me/advertising?ref=seller-platform-mcnav
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=etsy.com
+// @downloadURL  https://github.com/cengaver/EtsyScript/raw/refs/heads/main/openLinksSequentially.user.js
+// @updateURL    https://github.com/cengaver/EtsyScript/raw/refs/heads/main/openLinksSequentially.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function openLinks() {
+        const links = document.querySelectorAll("#manage_advertised_listings_wt_tab_panel > div > table > tbody > tr > td.wt-table__row__cell.wt-pr-xs-3.wt-text-left-xs.wt-table__row__cell.wt-display-table-cell.wt-pt-xs-2.wt-pb-xs-2.wt-no-wrap > div.wt-pt-xs-1.wt-display-flex-xs > div > a");
+
+        links.forEach((link, index) => {
+            setTimeout(() => {
+                //console.log(link.href)
+                window.open(link.href, '_blank');
+            }, index * 1000);
+        });
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.ctrlKey && event.altKey) {
+            openLinks();
+            console.log("yeni sekmeler çalıştı")
+        }
+    });
+    const observer = new MutationObserver(() => {
+        console.log("DOM changed, rechecking links...");
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
