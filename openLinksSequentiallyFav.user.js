e89b974042f6e749c84c6cca912f3b669e94d254
cengaver <selim_koyuncu@hotmail.com>
2025-04-23 01:42:00 +0300

    Update openLinksSequentiallyFav.user.js

--- openLinksSequentiallyFav.user.js
+++ openLinksSequentiallyFav.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Open Links Sequentially for ETSY Dashboard favorited people profile link
-// @version      1.2
+// @version      1.23
 // @description  Open all matching links with a 3-second delay
 // @namespace    https://github.com/cengaver
 // @author       Cengaver
-// @match        https://www.etsy.com/your/shops/me/dashboard?ref=seller-platform-mcnav
+// @match        https://www.etsy.com/your/shops/me/dashboard/activity
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=etsy.com
 // @downloadURL  https://github.com/cengaver/EtsyScript/raw/refs/heads/main/openLinksSequentiallyFav.user.js
 // @updateURL    https://github.com/cengaver/EtsyScript/raw/refs/heads/main/openLinksSequentiallyFav.user.js
@@ -15,22 +15,22 @@
     'use strict';
 
     function openLinks() {
-        const favHrefs = [...document.querySelectorAll("#recent-activity-content-region > ul > li")]
-        .filter(li => li.querySelector("span.wt-icon--smallest.etsy-icon"))
-        .map(li => li.querySelector('a[data-testid="username"]')?.href);
+        const favHrefs = [...document.querySelectorAll("#main-content > div > div.wt-width-full.wt-pr-xs-2.wt-pr-lg-6.wt-pl-xs-2.wt-pl-lg-6 > div.wt-mt-xs-6.wt-ml-xs-0 > a")]
+        .filter(a => a.querySelector("span.wt-fill-brick-dark"))
+        .map(a => a.querySelector('a[data-clg-id="WtTextLink"]')?.href);
         const links = [...new Set(favHrefs)];
 
         links.forEach((link, index) => {
             setTimeout(() => {
                 console.log(link)
                 window.open(link, '_blank');
-            }, index * 2000);
+            }, index * 3000);
         });
     }
 
     function clickUntilButtonDisappears(interval = 3000) {
         const intervalId = setInterval(() => {
-            const button = document.querySelector("#recent-activity-content-region > div > div > span > button");
+            const button = document.querySelector("#main-content > div > div.wt-width-full.wt-pr-xs-2.wt-pr-lg-6.wt-pl-xs-2.wt-pl-lg-6 > div.wt-mt-xs-6.wt-ml-xs-0 > button");
             if (button) {
                 button.click();
             } else {
